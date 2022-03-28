e4fb4c85676a6549ffaa3ef825e14e9db7e740ac
baptiste <baptiste.tielles@outlook.fr>
2022-03-28 22:03:55 +0200

    Log in/ Register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -31,6 +31,14 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
+            if($user->getAdministrateur()){
+                $user->setRoles(['ROLE_ADMIN']);
+            }else{
+                $user->setRoles(['ROLE_USER']);
+            }
+            $user->setActif(true);
+
+            //TODO: Formatage champs text !
 
             $entityManager->persist($user);
             $entityManager->flush();
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,9 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
 use App\Entity\Participant;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,14 +20,32 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
+            ->add('username',null,[
+                'required' => true,
+                'label' => 'Pseudo'
+            ])
+            ->add('lastname', null,[
+                'required'=>true,
+                'label' => 'Nom'
+            ])
+            ->add('firstname', null,[
+                'required'=>true,
+                'label' => 'Prénom'
+            ])
+            ->add('telephone', null,[
+                'required'=>true,
+                'label' => 'Téléphone'
+            ])
+            ->add('mail', null,[
+                'required'=>true,
+                'label' => 'Adresse Email'
+            ])
+            ->add('administrateur', null,[
+                'required'=>true
+            ])
+            ->add('campus', EntityType::class,[
+                'class' => Campus::class,
+                'choice_label' => 'nom',
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
@@ -42,6 +63,7 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
+                'label' => 'Mot de Passe',
             ])
         ;
     }
