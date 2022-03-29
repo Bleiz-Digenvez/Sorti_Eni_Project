7e42e6920feee3a7840f1425b3bc9c2aa1d5fa22
baptiste <baptiste.tielles@outlook.fr>
2022-03-29 09:32:22 +0200

    modify route

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -49,7 +49,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // TODO: return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        return new RedirectResponse($this->urlGenerator->generate('main_home'));
     }
 
     protected function getLoginUrl(Request $request): string
