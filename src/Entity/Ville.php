cc40b27d9c7150ddaf6ecb856fd62871b9dfe364
Edward Noel <ednoel59@gmail.com>
2022-04-07 18:33:36 +0200

    css + unique nom cp

--- src/Entity/Ville.php
+++ src/Entity/Ville.php
@@ -6,10 +6,12 @@ use App\Repository\VilleRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=VilleRepository::class)
+ * @UniqueEntity(fields={"nom","codePostal"}, errorPath="codePostal")
  */
 class Ville
 {
@@ -21,7 +23,7 @@ class Ville
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=50, unique=true)
+     * @ORM\Column(type="string", length=50)
      * @Assert\NotBlank
      */
     private $nom;
