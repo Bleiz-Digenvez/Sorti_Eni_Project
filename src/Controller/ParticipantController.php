644a284c8846a8dab42df7e6912f7f45d8b1ffc3
baptiste <baptiste.tielles@outlook.fr>
2022-04-08 09:41:56 +0200

    suppression Librairie

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -11,8 +11,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
-
 
 class ParticipantController extends AbstractController
 {
